b78f622400771c1da57b4086da8db967cd37eb2d
wisdomstar94 <wisdomstar94@gmail.com>
2023-05-13 23:27:30 +0900

    ZeroBaseTestPage 페이지 마이그레이션.

--- src/pages/test/zero-base/index.tsx
+++ src/pages/test/zero-base/index.tsx
@@ -1,52 +1,37 @@
-import { useDragAndDropController } from "@/hooks/use-drag-and-drop-controller/use-drag-and-drop-controller.hook";
-import { useDragAndDrop } from "@/hooks/use-drag-and-drop/use-drag-and-drop.hook";
-import { ICommon } from "@/interfaces/common.interface";
-import { useState } from "react";
+import { useDragAndDrop } from "@/hooks/new/use-drag-and-drop/use-drag-and-drop.hook";
+import { createRef } from "react";
 
 export default function ZeroBaseTestPage() {
-  const [aList, setAList] = useState<ICommon.Item[] | undefined>([
-    { name: 'a', value: 'a', },
-    { name: 'aaa', value: 'aaa', },
-    { name: 'aaaaa', value: 'aaaaa', },
-    { name: 'aaaaaaa', value: 'aaaaaaa', },
-  ]);
-
-  const [bList, setBList] = useState<ICommon.Item[] | undefined>([
-    // { name: 'b', value: 'b', },
-    // { name: 'bbb', value: 'bbb', },
-    // { name: 'bbbbb', value: 'bbbbb', },
-    // { name: 'bbbbbbb', value: 'bbbbbbb', },
-  ]);
-
-  const dndController = useDragAndDropController<ICommon.Item>({
-    onListsChange(map) {
-      if (map.has('aList')) {
-        setAList(map.get('aList'));
-      }
-      if (map.has('bList')) {
-        setBList(map.get('bList'));
-      }
-    },
-  });
-
-  const aListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'aList',
-    list: aList,
-    listLayout: {
-      type: 'fixed-col-count-grid',
-      fixedColCount: 2,
-    },
-  });
-
-  const bListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'bList',
-    list: bList,
-    listLayout: {
-      type: 'fixed-col-count-grid',
-      fixedColCount: 2,
-    },
+  const dnd = useDragAndDrop({
+    lists: [
+      [
+        'aList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [
+            { name: 'a', value: 'a', },
+            { name: 'aaa', value: 'aaa', },
+            { name: 'aaaaa', value: 'aaaaa', },
+            { name: 'aaaaaaa', value: 'aaaaaaa', },
+          ],
+          listLayout: {
+            type: 'fixed-col-count-grid',
+            fixedColCount: 2,
+          },
+        },
+      ],
+      [
+        'bList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [],
+          listLayout: {
+            type: 'fixed-col-count-grid',
+            fixedColCount: 2,
+          },
+        },
+      ],
+    ],
   });
 
   return (
@@ -54,9 +39,9 @@ export default function ZeroBaseTestPage() {
       <div className="w-full grid grid-cols-2 gap-2">
         <div>
           <div className="w-full box-border relative">
-            <div ref={aListDnD.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2">
+            <div ref={dnd.getList('aList')?.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2">
               {
-                aList?.map((item) => (
+                dnd.getList('aList')?.items.map((item) => (
                   <div key={item.value} className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white w-full">
                     <div data-is-dnd-handler={true} className="cursor-move">
                       :::
@@ -73,9 +58,9 @@ export default function ZeroBaseTestPage() {
 
         <div>
           <div className="w-full box-border relative">
-            <div ref={bListDnD.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2">
+            <div ref={dnd.getList('bList')?.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2">
               {
-                bList?.map((item) => (
+                dnd.getList('bList')?.items.map((item) => (
                   <div key={item.value} className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white w-full">
                     <div data-is-dnd-handler={true} className="cursor-move">
                       :::
