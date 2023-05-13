2455e708f145a572c37b0776ea7dc617a2f8024e
wisdomstar94 <wisdomstar94@gmail.com>
2023-05-13 23:34:07 +0900

    DragEffectTestPage 페이지 마이그레이션.

--- src/pages/test/drag-effect/index.tsx
+++ src/pages/test/drag-effect/index.tsx
@@ -1,36 +1,43 @@
-import { useDragAndDropController } from "@/hooks/use-drag-and-drop-controller/use-drag-and-drop-controller.hook";
-import { useDragAndDrop } from "@/hooks/use-drag-and-drop/use-drag-and-drop.hook";
-import { ICommon } from "@/interfaces/common.interface";
-import { useState } from "react";
+import { useDragAndDrop } from "@/hooks/new/use-drag-and-drop/use-drag-and-drop.hook";
+import { createRef } from "react";
 import styles from './index.module.scss';
 
 export default function DragEffectTestPage() {
-  const [aList, setAList] = useState<ICommon.Item[] | undefined>([
-    { name: 'a', value: 'a', },
-    { name: 'aaa', value: 'aaa', },
-    { name: 'aaaaa', value: 'aaaaa', },
-    { name: 'aaaaaaa', value: 'aaaaaaa', },
-  ]);
-
-  const [bList, setBList] = useState<ICommon.Item[] | undefined>([
-    { name: 'b', value: 'b', },
-    { name: 'bbb', value: 'bbb', },
-    { name: 'bbbbb', value: 'bbbbb', },
-    { name: 'bbbbbbb', value: 'bbbbbbb', },
-  ]);
-
-  const dndController = useDragAndDropController<ICommon.Item>({
-    onListsChange(map) {
-      if (map.has('aList')) {
-        setAList(map.get('aList'));
-      }
-      if (map.has('bList')) {
-        setBList(map.get('bList'));
-      }
-    },
-    onDestinationActiveListName(name) {
-      console.log('@name', name);
-    },
+  const dnd = useDragAndDrop({
+    lists: [
+      [
+        'aList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [
+            { name: 'a', value: 'a', },
+            { name: 'aaa', value: 'aaa', },
+            { name: 'aaaaa', value: 'aaaaa', },
+            { name: 'aaaaaaa', value: 'aaaaaaa', },
+          ],
+          listLayout: {
+            type: 'fixed-col-count-grid',
+            fixedColCount: 2,
+          },
+        },
+      ],
+      [
+        'bList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [
+            { name: 'b', value: 'b', },
+            { name: 'bbb', value: 'bbb', },
+            { name: 'bbbbb', value: 'bbbbb', },
+            { name: 'bbbbbbb', value: 'bbbbbbb', },
+          ],
+          listLayout: {
+            type: 'fixed-col-count-grid',
+            fixedColCount: 2,
+          },
+        },
+      ],
+    ],
     onStartDrag(dragFromInfo) {
       const fromItemElement = dragFromInfo?.targetItemElement;
       fromItemElement?.firstElementChild?.classList.add(styles['drag-from-item-effect']);
@@ -41,34 +48,14 @@ export default function DragEffectTestPage() {
     },
   });
 
-  const aListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'aList',
-    list: aList,
-    listLayout: {
-      type: 'fixed-col-count-grid',
-      fixedColCount: 2,
-    },
-  });
-
-  const bListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'bList',
-    list: bList,
-    listLayout: {
-      type: 'fixed-col-count-grid',
-      fixedColCount: 2,
-    },
-  });
-
   return (
     <>
       <div className="w-full grid grid-cols-2 gap-2">
         <div>
           <div className="w-full box-border relative">
-            <div ref={aListDnD.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2 h-screen items-start content-start">
+            <div ref={dnd.getList('aList')?.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2 h-screen items-start content-start">
               {
-                aList?.map((item) => (
+                dnd.getList('aList')?.items.map((item) => (
                   <div className="inline-flex" key={item.value}>
                     <div className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white w-full">
                       <div data-is-dnd-handler={true} className="cursor-move">
@@ -87,9 +74,9 @@ export default function DragEffectTestPage() {
 
         <div>
           <div className="w-full box-border relative">
-            <div ref={bListDnD.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2 h-screen items-start content-start">
+            <div ref={dnd.getList('bList')?.ref} className="bg-blue-200 p-2 pb-24 grid grid-cols-2 h-screen items-start content-start">
               {
-                bList?.map((item) => (
+                dnd.getList('bList')?.items.map((item) => (
                   <div className="inline-flex" key={item.value}>
                     <div className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white w-full">
                       <div data-is-dnd-handler={true} className="cursor-move">
