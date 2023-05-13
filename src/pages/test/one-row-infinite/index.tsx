f36bcb1854d738e1b04064defed54fd9ff0d80e8
wisdomstar94 <wisdomstar94@gmail.com>
2023-05-13 23:24:20 +0900

    OneRowInfiniteTestPage 페이지 마이그레이션

--- src/pages/test/one-row-infinite/index.tsx
+++ src/pages/test/one-row-infinite/index.tsx
@@ -1,59 +1,49 @@
-import { useDragAndDropController } from "@/hooks/use-drag-and-drop-controller/use-drag-and-drop-controller.hook";
-import { useDragAndDrop } from "@/hooks/use-drag-and-drop/use-drag-and-drop.hook";
-import { ICommon } from "@/interfaces/common.interface";
-import { useState } from "react";
+import { useDragAndDrop } from "@/hooks/new/use-drag-and-drop/use-drag-and-drop.hook";
+import { createRef } from "react";
 
 export default function OneRowInfiniteTestPage() {
-  const [aList, setAList] = useState<ICommon.Item[] | undefined>([
-    { name: 'aaaaaaaaaaaaaaa', value: 'a', },
-    { name: 'aaa', value: 'aaa', },
-    { name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', value: 'aaaaa', },
-    { name: 'aaaaaaa', value: 'aaaaaaa', },
-  ]);
-
-  const [bList, setBList] = useState<ICommon.Item[] | undefined>([
-    { name: 'bbbbbbbbbbbbbbb', value: 'b', },
-    { name: 'bbb', value: 'bbb', },
-    { name: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', value: 'bbbbb', },
-    { name: 'bbbbbbb', value: 'bbbbbbb', },
-  ]);
-
-  const dndController = useDragAndDropController<ICommon.Item>({
-    onListsChange(map) {
-      if (map.has('aList')) {
-        setAList(map.get('aList'));
-      }
-      if (map.has('bList')) {
-        setBList(map.get('bList'));
-      }
-    },
-  });
-
-  const aListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'aList',
-    list: aList,
-    listLayout: {
-      type: 'one-row-infinite',
-    },
-  });
-
-  const bListDnD = useDragAndDrop({
-    controller: dndController,
-    name: 'bList',
-    list: bList,
-    listLayout: {
-      type: 'one-row-infinite',
-    },
+  const dnd = useDragAndDrop({
+    lists: [
+      [
+        'aList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [
+            { name: 'aaaaaaaaaaaaaaa', value: 'a', },
+            { name: 'aaa', value: 'aaa', },
+            { name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', value: 'aaaaa', },
+            { name: 'aaaaaaa', value: 'aaaaaaa', },
+          ],
+          listLayout: {
+            type: 'one-row-infinite',
+          },
+        },
+      ],
+      [
+        'bList',
+        {
+          ref: createRef<HTMLDivElement>(),
+          items: [
+            { name: 'bbbbbbbbbbbbbbb', value: 'b', },
+            { name: 'bbb', value: 'bbb', },
+            { name: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', value: 'bbbbb', },
+            { name: 'bbbbbbb', value: 'bbbbbbb', },
+          ],
+          listLayout: {
+            type: 'one-row-infinite',
+          },
+        },
+      ],
+    ],
   });
 
   return (
     <>
       <div className="w-full relative">
         <div className="w-full box-border relative">
-          <div ref={aListDnD.ref} className="bg-blue-200 p-2 flex pr-20">
+          <div ref={dnd.getList('aList')?.ref} className="bg-blue-200 p-2 flex pr-20">
             {
-              aList?.map((item) => (
+              dnd.getList('aList')?.items.map((item) => (
                 <div key={item.value} className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white">
                   <div data-is-dnd-handler={true} className="cursor-move">
                     :::
@@ -72,9 +62,9 @@ export default function OneRowInfiniteTestPage() {
 
       <div className="w-full relative">
         <div className="w-full box-border relative">
-          <div ref={bListDnD.ref} className="bg-blue-200 p-2 flex pr-20">
+          <div ref={dnd.getList('bList')?.ref} className="bg-blue-200 p-2 flex pr-20">
             {
-              bList?.map((item) => (
+              dnd.getList('bList')?.items.map((item) => (
                 <div key={item.value} className="inline-flex flex-row gap-2 border border-slate-300 p-2 rounded-lg bg-white">
                   <div data-is-dnd-handler={true} className="cursor-move">
                     :::
